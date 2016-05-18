81fb806a915566be2e94b942df4cabc33c8b7da9
Rob Hailman <robhailman@gmail.com>
2016-05-18 15:47:43 -0400

    Remove reference to TabularInline
    Obsolete reference to TabularInline in admin was causing flake8 to
    barf.
    
    Fixed docstring while I'm at it.

--- djangocms_unitegallery/admin.py
+++ djangocms_unitegallery/admin.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.admin import TabularInline, StackedInline
+from django.contrib.admin import StackedInline
 from .models import GalleryPhoto
 
 
 class PhotoInline(StackedInline):
     """
-    Tabular inline that will be displayed in the gallery form during frontend
+    Stacked inline that will be displayed in the gallery form during frontend
     editing or in the admin site.
     """
     model = GalleryPhoto
